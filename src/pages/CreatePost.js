acd4819df992a571bdb4b6663fdf92aad733e5d0
Esteban Calderon <esteban.calderon@gmail.com>
2023-01-09 16:00:29 -0500

    First attempts to build a blog with React

---
+++ src/pages/CreatePost.js
@@ -0,0 +1,20 @@
+function CreatePost() {
+    return (
+        <div className="createPostPage">
+            <div className="cpContainer">
+                <h1>Create a Post</h1>
+                <div className="inputGp">
+                    <label>Title:</label>
+                    <input placeholder="Title..."/>
+                </div>
+                <div className="inputGp">
+                    <label>Post:</label>
+                    <textarea placeholder="Post..."></textarea>
+                </div>
+                <button>Submit Post</button>
+            </div>
+        </div>
+    );
+}
+
+export default CreatePost;
